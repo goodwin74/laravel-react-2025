c76e7e08539d6c6af7b048a725151223a9be790f
Sergey <serg1992@ya.ru>
2025-04-25 16:19:03 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover">
+        
         <title>Laravel </title>
         @vitereactrefresh
         @vite(['resources/js/app.js'])
